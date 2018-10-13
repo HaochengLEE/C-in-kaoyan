27cf274057db2d12c6ad39b3f5f75951cd2ad311
lihaocheng <lihaocheng00@126.com>
2018-10-13 23:51:29 +0800

    20181013

--- 6.cpp
+++ 6.cpp
@@ -1,27 +1,27 @@
 //
 // Created by 李昊城 on 2018/10/11.
+// 将一个整数中的每一位上为奇数的数依次取出，构成一个新数放在t中。高位仍在高位，低位仍在低位。例如，当s中的数为：87653142时，t中的数为：7531。
 //
 #include <stdio.h>
-float fun(int n){
-    int i,j;
-    float count=0;
-    for(i=1;i<=n;i++){
-        int sum=1;
-        for(j=i+1;j>=0;j--){
-            sum+=j;
-        }
-        count+=1.0/sum;
-
-
-    }
-    return count;
-}
+#include <math.h>
 int main(){
-    int a;
-    float b;
-    scanf("%d",&a);
-    b=fun(a);
-    printf("result is %.1f\n",b );
+    int a=87653142;
+//    scanf("%d",&a);
+    int t=0,k=0;
+
+    while (a!=0){
+
+        if(a%10%2!=0){
+            t+=a%10*pow(10,k);
+            k++;
+
+
+        }
+        a=a/10;
+    }
+
+
+    printf("result is %d\n",t);
     return 0;
 
 }
